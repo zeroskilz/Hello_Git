4a8fad249d6170e3fd39b89ea9182e393e6b3d09
zeroNULL <zeroskilz@mail.com>
2017-11-26 18:56:24 -0500

    Delete download.sh

--- bash-hello/download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-array=('https://github.com/zeroskilz/test'  'https://github.com/zeroskilz/test/tree/tester'  'https://github.com/zeroskilz/Hello_Git'  'https://github.com/mozilla-services/syncserver'  'https://github.com/torvalds/test-tlb' )
-
-#for lines in "${array[@]}; do adb pull /mnt/sdcard/Download/${array[$lines]} ;done
-mkdir ~/tmp
-cd ~/tmp
-for lines in "${!array[@]}"; do git clone ${array[$lines]}; done; 
